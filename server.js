d36153001dba29c773d859dfd67079ed77f856c6
GabrielGoncalves0 <bie.gabriel1@outlook.com>
2024-11-01 11:38:56 -0300

    Feat: add Controllers and Repositories

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const outputJson = require("./swagger-output.json");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(outputJson))
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(outputJson));
 
 app.listen(5000, function () {
     console.log("servidor web em funcionamento!");
